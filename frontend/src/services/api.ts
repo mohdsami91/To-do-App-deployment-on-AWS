6958e87524e4672e7db8eea5570d013fd760e7b0
mohdsami91 <150219542+mohdsami91@users.noreply.github.com>
2025-02-23 02:16:27 +0530

    changed api.ts line 5

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Todo, NewTodo, UpdateTodo } from '../types';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'http://3.84.39.52:3000/api'
 });
 
 export const getTodos = async (): Promise<Todo[]> => {
